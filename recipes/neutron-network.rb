4f72a4e5f0792844a742a4bd45ab1f72bca952c4
Paul Sroufe <psroufe@softlayer.com>
2013-10-15 11:10:46 -0500

    Update to naming for Github release.

--- recipes/neutron-network.rb
+++ recipes/neutron-network.rb
@@ -153,6 +153,6 @@ end
 
 
 #Use the neutron LWRP when you need multiple L3 routers configured.  A router for each softlayer public and private networks
-# grizzly_neutron "Setup softlayer L3 router config for openstack" do
+# chef-openstack_neutron "Setup softlayer L3 router config for openstack" do
 # 	action :softlayer_l3_config
 # end
